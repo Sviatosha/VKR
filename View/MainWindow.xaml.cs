b15a6db79cd8e45dd179127c1f5702fc85b241f9
Sviatosha <spirin.anton@list.ru>
2024-06-07 23:16:18 +0400

    Оптимизация меню + Сделанные уроки

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,26 +1,16 @@
 ﻿using System;
-using System.Data;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections;
-using static System.Net.WebRequestMethods;
-using System.Reflection.Emit;
 using VKR.Models;
-using System.Printing;
 using System.Windows.Threading;
-using static System.Net.Mime.MediaTypeNames;
+using System.Media;
+using WMPLib;
+using VKR.View.Lessons;
+using VKR.View.About;
+using VKR.View.Help;
 
 namespace VKR.View
 {
@@ -36,38 +26,51 @@ namespace VKR.View
             TextTape tape = new TextTape();
             myTextTape.Add(tape);
             InitializeComponent();
-            initiateExercise();
-            Wait();
-
-            timerCreate();
+            initiateExercise();//
+            Wait();// 
+            timerCreate();// Создать таймер
 
         }
-        private static TextTapeSingleton myTextTape = TextTapeSingleton.GetInstance();
-        private DispatcherTimer timer = null;
-        private int seconds, clicks;
+        private static TextTapeSingleton myTextTape = TextTapeSingleton.GetInstance();//singleton
+        private DispatcherTimer timer = null;// Обьявление таймера
+        private int seconds, clicks;//секунды и нажатия
 
-        private void timerCreate()
+        /*
+         * Обработка открытия новых окон
+         */
+
+        
+        /*
+        * Обработка таймера
+        */
+        private void timerCreate() //Создание таймера
         {
             timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) }; // 1 секунда
             timer.Tick += timerTick;
             timerForm();
         }
-        private void timerStart()
+        private void timerStart() //Запуск таймера
         {
             timer.Start();
             timerForm();
         }
-        private void timerEnd()
+        private void timerEnd() //Остановка таймера
         {
             timer.Stop();
             seconds = 0;
         }
-        private void timerTick(object sender, EventArgs e)
+
+        private void timerBackwards() // WIP Запуск таймера обратного отчёта
+        {
+            timer.Stop();
+            seconds = 0;
+        }
+        private void timerTick(object sender, EventArgs e) // Обработка изменения времени в таймере
         {
             seconds++;
             timerForm();
         }
-        private void timerForm()
+        private void timerForm() // Обработка изменения времени в таймере
         {
             var ts = TimeSpan.FromSeconds(seconds);
 
@@ -78,17 +81,12 @@ namespace VKR.View
             Timer.Text = timeInterval;
 
         }
-        
-        private void Wait()//Экран ожидания
-        {
-            myTextTape[0].textTapeWait();
-            Tape.Text = myTextTape[0].Tape;
-        }
-        private void KeyEvents(object sender, KeyEventArgs e)
-        {
 
-        }
-        private void startLetters()
+        /*
+        * Обработка Текстовой ленты
+        */
+
+        private void startLetters() // Инициализация нажатой и следующей буквы
         {
             string pressedLetter, letterToPress;
             letterToPress = myTextTape[0].Tape[10].ToString();
@@ -101,12 +99,32 @@ namespace VKR.View
             TapeNextLetters.Text = $"Следующая клавиша: {letterToPress}";
             TapeInputLetters.Text = $" Нажатая клавиша: ";
         }
-        private void TextInputEvent(object sender, TextCompositionEventArgs e)
+
+        private void changeVision()// Видимость нажатой и следующей буквы
+        {
+            if (myTextTape[0].IsTaping == true)
+            {
+                TapeNextLetters.Visibility = Visibility.Visible;
+                TapeInputLetters.Visibility = Visibility.Visible;
+                timerStart();
+            }
+            else if (myTextTape[0].IsTaping == false)
+            {
+                TapeNextLetters.Visibility = Visibility.Hidden;
+                TapeInputLetters.Visibility = Visibility.Hidden;
+                timerEnd();
+            }
+        }
+        private void Wait() //Экран ожидания
+        {
+            myTextTape[0].textTapeWait();
+            Tape.Text = myTextTape[0].Tape;
+        }
+        private void TextInputEvent(object sender, TextCompositionEventArgs e) //обработка нажатия для текстовой ленты
         {
             if (myTextTape[0].Tape[10].ToString() != e.Text)
             {
                 TapeInputLetters.Foreground = Brushes.Red;
-
             }
             else
             {
@@ -132,42 +150,11 @@ namespace VKR.View
             Tape.Text = myTextTape[0].Tape;
             clicks++;
             changeVision();
+            soundPlay();
         }
 
-        private void changeVision()
+        private void loadFile() //загрузка своего файла
         {
-            if (myTextTape[0].IsTaping == true)
-            {
-                TapeNextLetters.Visibility = Visibility.Visible;
-                TapeInputLetters.Visibility = Visibility.Visible;
-                timerStart();
-            }
-            else if (myTextTape[0].IsTaping == false)
-            {
-                TapeNextLetters.Visibility = Visibility.Hidden;
-                TapeInputLetters.Visibility = Visibility.Hidden;
-                timerEnd();
-            }
-        }
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            MenuItem menuItem = (MenuItem)sender;
-            if (menuItem.Header.ToString() == "Свой текст")
-            {
-                loadFile();
-            }
-            else if (menuItem.Header.ToString() == "Упражнения")
-            {
-                
-            }
-            else
-            {
-                getExercise(menuItem.Header.ToString());
-            }
-        }
-        private void loadFile()
-        {
-
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.FileName = "Document"; // Default file name
             dlg.DefaultExt = ".txt"; // Default file extension
@@ -182,34 +169,169 @@ namespace VKR.View
                 // Open document
                 string filename = dlg.FileName;
                 initiateTape(filename);
-
             }
         }
-        private void initiateTape(string textPath)
+        private void initiateTape(string textPath) //инициализация Текстовой ленты
         {
             myTextTape[0].initiateFileTape(textPath);
             Tape.Text = myTextTape[0].Tape;
             startLetters();
             timerStart();
         }
-        private void initiateExercise()//WIP
+
+        /*
+        * Обработка переключателей и меню
+        */
+        private void Trainer_Checked(object sender, RoutedEventArgs e) // тренажёры
         {
-            string[] allfiles = Directory.GetFiles(@"..\..\..\src\Exercises");
-            foreach (string filename in allfiles)
-            {
-                MenuItem exerc = new MenuItem();
-                exerc.Header = filename;
-                exerc.Click += MenuItem_Click;
-                ExerciseMenu.Items.Add(exerc);
-            }
-            
+            RadioButton pressed = (RadioButton)sender;
+            //MessageBox.Show(pressed.Content.ToString());
         }
-        private void getExercise(string Path)
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e) // Меню WIP
         {
-            myTextTape[0].getExercise(Path);
-            Tape.Text = myTextTape[0].Tape;
-            startLetters();
-            timerStart();
+            MenuItem menuItem = (MenuItem)sender;
+
+            if (menuItem.Header.ToString() == "Свой текст")
+            {
+                loadFile();
+            }
+            else if (menuItem.Header.ToString() == "Работа над ошибками")
+            {
+                //ErrorWork();
+            }
+            else if (menuItem.Header.ToString() == "Статистика")
+            {
+                StatisticWindow statisticWindow = new StatisticWindow();
+                statisticWindow.Show();
+            }
+            else if (menuItem.Header.ToString() == "Настройки")
+            {
+                Settings settingsWindow = new Settings();
+                settingsWindow.Show();
+            }
+            else if (menuItem.Header.ToString() == "Помощь")
+            {
+                HelpWindow helpWindow = new HelpWindow();
+                helpWindow.Show();
+            }
+            else if (menuItem.Header.ToString() == "О программе")
+            {
+                AboutWindow aboutWindow = new AboutWindow();
+                aboutWindow.Show();
+            }
+        }
+        
+        private void Lessons_Click(object sender, RoutedEventArgs e) // Меню WIP
+        {
+            MenuItem menuItem = (MenuItem)sender;
+
+            if (menuItem.Header.ToString() == "Что такое метод слепой печати")
+            {
+                LessonsBook LessonsWindow = new LessonsBook();
+                LessonsWindow.Show();
+            }
+            else if (menuItem.Header.ToString() == "Перед печатью")
+            {
+
+            }
+            else if (menuItem.Header.ToString() == "Метод десятепальцевой слепой печати")
+            {
+
+            }
+            else if (menuItem.Header.ToString() == "Для людей с ограниченными возможностями")
+            {
+
+            }
+        }
+        private void Exercises_Click(object sender, RoutedEventArgs e) // Меню WIP
+        {
+            MenuItem menuItem = (MenuItem)sender;
+
+            getExercise(menuItem.Header.ToString());
+
+        }
+        private void initiateExercise()//WIP инициировать упражнение
+        {
+            string path = @"..\..\..\src\Exercises";
+
+            string[] languagueDirectories = Directory.GetDirectories(path);
+            foreach (string ldirectory in languagueDirectories)
+            {
+                MenuItem lang = new MenuItem();
+                string langName = System.IO.Path.GetFileName(ldirectory);
+                lang.Header = langName;
+
+                string[] allDerectories = Directory.GetDirectories(ldirectory);
+                foreach (string directory in allDerectories)
+                {
+                    MenuItem dir = new MenuItem();
+                    string dirName = System.IO.Path.GetFileName(directory);
+                    dir.Header = dirName;
+                    string[] allfiles = Directory.GetFiles(directory);
+
+                    foreach (string filename in allfiles)
+                    {
+                        MenuItem exerc = new MenuItem();
+                        string fName = System.IO.Path.GetFileName(filename);
+
+                        exerc.Header = fName;
+                        exerc.Click += Exercises_Click;
+
+                        dir.Items.Add(exerc);
+                    }
+                    lang.Items.Add(dir);
+                }
+                ExerciseMenu.Items.Add(lang);
+            }
+        }
+        private void getExercise(string fileName) //инициализация Упражнений
+        {
+            string path = @"..\..\..\src\Exercises";// Путь к Директории Exercises
+
+            string[] languagueDirectories = Directory.GetDirectories(path);
+            foreach (string ldirectory in languagueDirectories)
+            {
+                string languagueName = System.IO.Path.GetFileName(ldirectory);
+                string[] allDirectories = Directory.GetDirectories(ldirectory); // Список путей к папкам в директории Exercises
+
+                foreach (string directory in allDirectories)// Для каждой подпапки Exercises
+                {
+                    string dirName = System.IO.Path.GetFileName(directory);// Путь к подпапке Exercises
+                    string[] allfiles = Directory.GetFiles(directory);// Список путей к подпапке в подпапке директории Exercises
+                    foreach (string filename in allfiles)
+                    {
+                        string fName = System.IO.Path.GetFileName(filename);
+
+                        if (fName == fileName)
+                        {
+                            myTextTape[0].getExercise(filename);
+                            Tape.Text = myTextTape[0].Tape;
+                            startLetters();
+                            timerStart();
+                        }
+                    }
+                }
+            }
+        }
+
+        /*
+        * Обработка клавиатуры
+        */
+        private void KeyEvents(object sender, KeyEventArgs e)
+        {
+
+        }
+        /*
+        * Обработка звуков
+        */
+        private void soundPlay() //
+        {
+            WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+            var fullPath = Path.GetFullPath(@"..\..\..\src\sounds\keyboardClick.mp3");
+
+            wmp.URL = fullPath;
+            wmp.controls.play();
         }
     }
 }
